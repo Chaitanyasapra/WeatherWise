03e182242d4091fdd89f8b77934939213942ea97
Chaitanya <anuweets28@gmail.com>
2024-09-08 13:49:55 +0530

    Initial commit - WeatherWise app

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Created by
+      <a href="https://github.com/Chaitanyasapra"
+        > Chaitanya Sapra, Harsh Sharma, Sohil Dhiman</a
+      > 
+    </p>
+  </div>
+  );
+}
